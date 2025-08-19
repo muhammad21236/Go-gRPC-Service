02557cdd45c0bdb4028202b5d8e2187105290ba4
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-08-19 16:48:58 +0500

    Project Setup gRPC Services

---
+++ cmd/server/main.go
@@ -0,0 +1,13 @@
+package main
+
+func Run() error {
+	// Responsible for initializing the gRPC server and starting it.
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		panic(err)
+	}
+	
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/muhammad21236/Go-gRPC-Service
+
+go 1.24.0
