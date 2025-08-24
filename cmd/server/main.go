19d40a34d8ecbc18f351caa221b2687af51baee8
Muhammad <155086759+muhammad21236@users.noreply.github.com>
2025-08-24 12:56:30 +0500

    Adding a Database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"github.com/muhammad21236/Go-gRPC-Service/internal/db"
+	"github.com/muhammad21236/Go-gRPC-Service/internal/rocket"
+)
+
 func Run() error {
 	// Responsible for initializing the gRPC server and starting it.
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	_ = rocket.New(rocketStore)
 	return nil
 }
 
@@ -9,5 +20,5 @@ func main() {
 	if err := Run(); err != nil {
 		panic(err)
 	}
-	
+
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/mock v1.6.0 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	go.uber.org/mock v0.6.0 // indirect
